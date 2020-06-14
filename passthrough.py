3e2fc75542c431c835faba3e2193c5f54fe766ae
Albin Severinson <albin@severinson.org>
2020-06-14 13:58:08 +0200

    write EOF at exit, name log files after the command and arguments given

--- passthrough.py
+++ passthrough.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-# stdout/stdin passthrough and logging
+
+# Script for logging stdin/stdout of a separate process. Useful to
+# monitor an application that is automated via its CLI. Runs the
+# argument(s) given to this script as a separate process, while
+# passing stdin and stdout through and writing them to files.
 
 import os
 import sys
 import subprocess
 
+# command to run
 cmd = "./echo.py"
+
 r_in, w_in = os.pipe()
 r_out, w_out = os.pipe()
 processid = os.fork()
@@ -13,7 +19,7 @@ if processid: # process responsible for logging
     processid = os.fork()
     if processid: # process responsible for stdin
         w_in = os.fdopen(w_in, "w")
-        stdin_file = open("stdin.txt", "w")
+        stdin_file = open(cmd + " " + " ".join(sys.argv[1:]) + " stdin.txt", "w")
         for line in sys.stdin:
             line = line.strip()
             print(line, file=stdin_file) # write to file
@@ -28,7 +34,7 @@ if processid: # process responsible for logging
         sys.exit(0)
     else:
         r_out = os.fdopen(r_out)
-        stdout_file = open("stdout.txt", "w")
+        stdout_file = open(cmd + " " + " ".join(sys.argv[1:]) + " stdout.txt", "w")
         for line in r_out:
             line = line.strip()
             print(line, file=sys.stdout) # write to stdout
@@ -40,6 +46,7 @@ if processid: # process responsible for logging
         os.close(r_out)
         os.close(w_out)
         stdout_file.close()
+        sys.stdout.close()
         sys.exit()
 else: # process responsible for running cmd
     subprocess.run([cmd] + sys.argv[1:], stdin=os.fdopen(r_in), stdout=os.fdopen(w_out, "w"))
